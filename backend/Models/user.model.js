f7049b076ca3dc12a05b041063aab67a3f97f18c
Prateek Kumar <prateekkumar72007@gmail.com>
2025-01-21 19:09:57 +0530

    fix: implement user creation and add gitignore

--- backend/Models/user.model.js
+++ backend/Models/user.model.js
@@ -1,57 +1,57 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt.js"
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema({
-name: {
+  name: {
     type: String,
-    required: [true,"Name is Required"]
-},
-email: {
+    required: [true, "Name is Required"],
+  },
+  email: {
     type: String,
-    required: [true,"Email is Required"],
+    required: [true, "Email is Required"],
     unique: true,
     lowercase: true,
-    trim: true
-},
-password : {
+    trim: true,
+  },
+  password: {
     type: String,
-    required: [true,"Password is Required"],
-    minlength: [6,"Password must be of length 6"]
-},
-cartItems: {
-    quantity:{
-        type: Number,
-        default: 1
+    required: [true, "Password is Required"],
+    minlength: [6, "Password must be of length 6"],
+  },
+  cartItems: {
+    quantity: {
+      type: Number,
+      default: 1,
     },
-    product:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        }
-},
-role: {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  role: {
     type: String,
-    enum: ['customer', 'admin'],
-    default: 'customer'
-},
-//  createdAt , updatedAt
-timestamps: true
+    enum: ["customer", "admin"],
+    default: "customer",
+  }},
+  //  createdAt , updatedAt
+  {timestamps: true,
+});
 
-})
-
-const User = mongoose.model("User",userSchema)
 // Pre-save hook to hash password before saving password
 userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
-   try{
-    const salt = await bcrypt.genSalt( 10);
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
-} catch(error){
-next(error);
-}
-   
-})
+  } catch (error) {
+    next(error);
+  }
+});
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-    
-}
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+
 export default User;
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,33 @@
+import User from "../Models/user.model.js";
 export const signup = async (req,res) =>{
     // desktop App => postman
+    try{
     const {email,password,name} = req.body;
-    res.send("Sign Up Route called");
+    const userExists = await User.findOne({email});
+    if(userExists){
+        return res.status(400).json({
+            message: "User Already Exists"
+        });
+    }
+    const user = new User({
+        name,
+        email,
+        password
+    });
+    await user.save();
+
+    res.status(400).json({"message":"Sign Up Route called"});
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).json({
+            message: "Something went wrong"
+        });
+    }
 }
 export const signin = async (req,res) =>{
     res.send("Sign In Route called");
 }
 export const logout = async (req,res) =>{
     res.send("Log Out Route called");
-}
+} 
