a8cb36eb7cb970259809feac746144788cb22fed
Prateek Kumar <prateekkumar72007@gmail.com>
2025-03-12 23:23:14 +0530

    feat: add coupon validation functionality and update routes

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -1,10 +1,38 @@
+import Coupon from "../Models/coupon.modal.js"
 export const getCoupon = async (req,res)=>{
     try {
-        const coupan = await Coupan.findone({userId:req.user._id,isActive: true})
-        res.json(coupan|| null)
+        const coupon = await Coupon.findone({userId:req.user._id,isActive: true})
+        res.json(coupon|| null)
     } catch (error) {
         console.log("Error in getCoupon Controller")
         res.status(500).json({message:"Server Error",error:error.message});
 
+    }
+}
+
+export const validateCoupon = async (req,res)=> {
+    try {
+        const {code} = req.body;
+        const coupon = await Coupon.findone({code:code,userId:req.user._id, isActive:true})
+        if (!coupon) {
+            res.status(400).json({
+                message:"Coupon Not Found"
+            })
+        };
+
+        if (coupon.expirationDate< new Date()) {
+            coupon.isActive = false;
+            return res.status(404).json({message:"Coupon Expired"})
+        };
+
+        res.json({
+            message:"Coupon is Valid",
+            code: Coupon.code,
+            discountPercentage:coupon.discountPercentage
+        })
+
+    } catch (error) {
+        console.log("error in coupon validation controller");
+        res.status(500).json({message:"server error",error: error.message})
     }
 }
--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,6 +1,10 @@
 import express from express;
-import { protectRoute } from "../Middlewares/auth.middleware";
+import { protectRoute } from "../Middlewares/auth.middleware.js";
+import { getCoupon } from "../controllers/coupon.controller.js";
 const router = express.router();
 
 router.get("/",protectRoute,getCoupon);
-router.get("/",protectRoute,getCoupon);
+router.get("/validate",protectRoute,validateCoupon);
+
+
+export default router
