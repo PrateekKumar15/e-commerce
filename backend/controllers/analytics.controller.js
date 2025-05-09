803285ec9380cdc20a8163b10315c4b1976c2a86
Prateek Kumar <prateekkumar72007@gmail.com>
2025-05-09 21:36:16 +0530

    refactor: Update model imports and improve code formatting; enhance coupon validation and payment processing logic

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,16 +1,19 @@
-import Order from "../Models/order.model.js";
-import Product from "../Models/product.model.js";
-// import User from "../models/user.model.js";
+import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
+import Order from "../models/order.model.js";
 
 export const getAnalyticsData = async () => {
+  // here req,res is not written because it is not the controller function but it is a function inside it
+
   const totalUsers = await User.countDocuments();
   const totalProducts = await Product.countDocuments();
 
   const salesData = await Order.aggregate([
     {
       $group: {
-        _id: null, // it groups all documents together,
-        totalSales: { $sum: 1 },
+        //The _id field specifies the key for grouping. If set to null, it means all documents are grouped together.
+        _id: null, //it groups all documents together
+        totalSales: { $sum: 1 }, //1 means that one is assigned to each document and so 1 is added to get totaal number of sales
         totalRevenue: { $sum: "$totalAmount" },
       },
     },
@@ -33,10 +36,14 @@ export const getDailySalesData = async (startDate, endDate) => {
   try {
     const dailySalesData = await Order.aggregate([
       {
+        //Filters documents based on the createdAt field.
+        //Ensures only orders within the specified startDate and endDate range are processed.
+        //This converts the createdAt field (a Date object) into a string format, such as YYYY-MM-DD.
+        //This allows grouping by the day.
         $match: {
           createdAt: {
-            $gte: startDate,
-            $lte: endDate,
+            $gte: startDate, //greater than
+            $lte: endDate, //less than
           },
         },
       },
@@ -47,27 +54,27 @@ export const getDailySalesData = async (startDate, endDate) => {
           revenue: { $sum: "$totalAmount" },
         },
       },
-      { $sort: { _id: 1 } },
+      { $sort: { _id: 1 } }, //ascending order sorting
     ]);
 
-    // example of dailySalesData
+    //example of dailySalesData
     // [
-    // 	{
-    // 		_id: "2024-08-18",
-    // 		sales: 12,
-    // 		revenue: 1450.75
-    // 	},
+    //     {   // lts say this is for monday and same type for tuesday and so on
+    //     _id: "2024-08-19",
+    //     sales: 12,
+    //     revenue: 1452.02
+    //     },
     // ]
 
     const dateArray = getDatesInRange(startDate, endDate);
-    // console.log(dateArray) // ['2024-08-18', '2024-08-19', ... ]
+    //console.log(dateArray);   // ['2024-08-18', '2024-08-19'....]
 
     return dateArray.map((date) => {
       const foundData = dailySalesData.find((item) => item._id === date);
 
       return {
         date,
-        sales: foundData?.sales || 0,
+        sales: foundData?.sales || 0, //?. is chaining operator so that no errors are thrown but instead undefined is returned
         revenue: foundData?.revenue || 0,
       };
     });
