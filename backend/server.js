e2a0e7c5da20fb244bb4e6dd2327bf980ee2326d
Prateek Kumar <prateekkumar72007@gmail.com>
2025-05-10 11:07:59 +0530

    fix: Add CORS middleware to enable cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Import the cors package
 
 //routes
 import authRoutes from "./routes/auth.route.js";
@@ -20,6 +21,12 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from your frontend origin
+    credentials: true, // Allow cookies to be sent
+  })
+);
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
